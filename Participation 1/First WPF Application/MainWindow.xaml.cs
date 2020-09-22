acbbc625704a69f3a714390943c6d90c0df68e23
shayo <shayo@DESKTOP-OPA1O5H>
2020-09-22 17:08:55 -0500

    Commit test

---
+++ Participation 1/First WPF Application/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace First_WPF_Application
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btn1_IsMouseDirectlyOverChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            btn1.Background = new SolidColorBrush(Colors.Red);
+            MessageBox.Show("This is where the calculated age would go");
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            int age = DateTime.Today.Year - dtpDate.SelectedDate.Year;
+            //I found some examples online and could not figure out how the syntax on how to get the year from the selected date.
+            //This is what I used-->  stackoverflow.com/questions/2237587/how-can-i-calculate-age-by-datetimepicker 
+
+
+        }
+    }
+}
---
+++ Participation 2-WPF/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Participation_2_WPF
+{
+    class EntryForm
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
+    
+        public EntryForm()
+        {
+            Name = string.Empty;
+            Address = string.Empty;
+            ZipCode = 1;
+        }
+        public EntryForm(string name, string address, int zipCode)
+        {
+            this.Name = name;
+            this.Address = address;
+            this.ZipCode = zipCode;
+            
+        }
+        public override string ToString()
+        {
+            return (Name + ", " +Address +", " +ZipCode);
+        }
+        
+    }
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnApplication_Click(object sender, RoutedEventArgs e)
+        {
+            EntryForm entryForm = new EntryForm();
+            entryForm.Name = txtName.Text;
+            entryForm.Address = txtAddress.Text;
+            entryForm.ZipCode = txtZip.Text;
+            lboxApplication.Items.Add(entryForm);
+            
+
+        }
+    }
+}
